7fedbdd9b81b74e9116ecfa63c770e7ad3a7acea
johnny-dash <maodi101@gmail.com>
2017-10-02 17:52:59 +1100

    add express

---
+++ app.js
@@ -0,0 +1,18 @@
+var express 	= require('express');
+var app 		= express();
+var http 		= require('http');
+var httpServer 	= http.createServer(app);
+var PORT 		= 80;
+
+
+httpServer.listen(PORT, function(){
+    console.log('HTTP Server is running on: http://localhost:%s', PORT);
+});
+
+
+// Welcome
+app.get('/', function(req, res) {
+    res.status(200).send('Welcome!');
+});
+
+module.export = app;
