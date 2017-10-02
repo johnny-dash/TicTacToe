f2b1c61a21fe3b0c3fe4956902e7c37acb95c4b0
johnny-dash <maodi101@gmail.com>
2017-10-02 18:22:45 +1100

    responsive to mobile

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express 	= require('express');
 var app 		= express();
 var http 		= require('http');
+
+app.use(express.static('build'));
+
 var httpServer 	= http.createServer(app);
 var PORT 		= 80;
 
